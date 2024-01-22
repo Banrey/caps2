74bb00c636317b2f2d2c9a3978ba9a6da852a2d2
Banrey <114892207+Banrey@users.noreply.github.com>
2024-01-22 14:55:20 +0800

    process.personalDash fix

--- process.personalDash.php
+++ process.personalDash.php
@@ -5,9 +5,14 @@
 			mysqli_stmt_bind_param($statement_check, "is", $loginCode, $email);
 			
 			$loginCode = NULL;
+            if(isset($_SESSION['email_address'])){
+
 			$email = $_SESSION['email_address'];			
 			str_replace("@","_",$email);
 			mysqli_stmt_execute($statement_check);
+            } else{
+                exit();
+            }
             
 			
         
